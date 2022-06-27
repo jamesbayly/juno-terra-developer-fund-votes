8580a54b0c1e7349f99fb2c083bf6c12044d8d29
James Bayly <bayly.james@gmail.com>
2022-06-27 15:27:02 +0200

    Formatting fix

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -9,7 +9,7 @@ type Msg = {
 };
 
 export async function handleTerraDeveloperFund(
-  message: CosmosMessage<{ sender: string; msg: Msg; }>
+  message: CosmosMessage<{ sender: string; msg: Msg }>
 ): Promise<void> {
   // logger.info(JSON.stringify(message));
   // Example vote https://www.mintscan.io/juno/txs/EAA2CC113B3EC79AE5C280C04BE851B82414B108273F0D6464A379D7917600A4
@@ -19,8 +19,8 @@ export async function handleTerraDeveloperFund(
     blockHeight: BigInt(message.block.block.header.height),
     voter: message.msg.decodedMsg.sender,
     proposalID: message.msg.decodedMsg.msg.vote.proposal_id,
-    vote: message.msg.decodedMsg.msg.vote.vote === 'yes',
-  })
+    vote: message.msg.decodedMsg.msg.vote.vote === "yes",
+  });
 
   await voteRecord.save();
 }
